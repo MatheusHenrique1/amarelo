ecf7ea5a5b1d5001e823e53a6a94c7de232c30af
MatheusHenrique1 <luizfragoso583@gmail.com>
2026-04-06 01:09:14 -0300

    Create Atividade_5.py

---
+++ Atividade_5.py
@@ -0,0 +1,7 @@
+#Leia um número como texto (input) e: Converta para inteiro; Mostre o triplo
+numero = input("Digite o valor: ")
+inteiro = int(numero)
+triplo = inteiro * 3
+
+print("O valor convertido em inteiro é: ",inteiro)
+print("O valor triplicado é: ",triplo)
