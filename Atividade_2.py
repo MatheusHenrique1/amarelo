266ce040edbbe5637f245f62e16aab6317f19e34
MatheusHenrique1 <luizfragoso583@gmail.com>
2026-04-06 00:42:55 -0300

    Create Atividade_2.py

---
+++ Atividade_2.py
@@ -0,0 +1,7 @@
+#Leia um número (entrada padrão do input) e: Converta para inteiro; Exiba o dobro do valor
+numero = input("Digite o número: ")
+inteiro = int(numero)
+
+dobro = inteiro * 2
+
+print("O valor dobrado é: ",dobro)
