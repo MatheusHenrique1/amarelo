dc2a3186b262a1317a5a19d22ec22d31aa2f35da
MatheusHenrique1 <luizfragoso583@gmail.com>
2026-04-06 01:08:59 -0300

    Create Atividade_7.py

---
+++ Atividade_7.py
@@ -0,0 +1,6 @@
+#Leia um valor e verifique: Se é maior que 10 → “Maior que 10”; Caso contrário → “Menor ou igual a 10"
+valor = int(input("Digite o valor: "))
+if valor > 10:
+    print("O valor é maior que 10.")
+else:
+    print("O valor é menor ou igual a 10.")
