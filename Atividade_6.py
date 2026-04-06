433836c5e70e0a5cfc0c018aebb5f272b78bb457
MatheusHenrique1 <luizfragoso583@gmail.com>
2026-04-06 01:08:52 -0300

    Create Atividade_6.py

---
+++ Atividade_6.py
@@ -0,0 +1,10 @@
+#Leia dois números e exiba qual é o maior.
+n1 = int(input("Digite o numero 1: "))
+n2 = int(input("Digite o numero 2: "))
+
+if (n1 > n2):
+    print("O número 1 é maior.")
+elif (n2 > n1):
+    print("O número 2 é maior.")
+else:
+    print("Os números são iguais!!")
