21e651be9c8236c452a44d0a4b8ff9b129514e57
MatheusHenrique1 <luizfragoso583@gmail.com>
2026-04-06 00:45:39 -0300

    Create Atividade_3.py

---
+++ Atividade_3.py
@@ -0,0 +1,5 @@
+#Leia um valor e: Mostre o valor digitado; Mostre o tipo da variável usando type
+numero = int(input("Digite o valor: "))
+
+print("O valor digitado foi: ",numero)
+print("O tipo de variavél é: ", type(numero))
